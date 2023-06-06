cec50e8613b761852b447b9c8cb93980d4693ee9
BachTDX <truongdoxuanbach@gmail.com>
2023-06-06 22:55:48 +0700

    [CreateUser] add api create user and unit test

---
+++ test/controllers/api/v1/users_controller_test.rb
@@ -0,0 +1,46 @@
+require "test_helper"
+
+class Api::V1::UsersControllerTest < ActionDispatch::IntegrationTest
+  
+  test 'should create a new user' do
+    post '/api/v1/users', params: {
+      user: {
+        name: 'John Doe',
+        email: 'john.doe@example.com',
+        password: 'password'
+      }
+    }
+
+    assert_response :created
+    assert_equal 'User created successfully', JSON.parse(response.body)['message']
+  end
+
+  test 'should return errors when creating an invalid user' do
+    post '/api/v1/users', params: {
+      user: {
+        name: '',
+        email: '',
+        password: ''
+      }
+    }
+
+    assert_response :unprocessable_entity
+    assert_includes JSON.parse(response.body)['error'], "Name can't be blank"
+    assert_includes JSON.parse(response.body)['error'], "Email can't be blank"
+    assert_includes JSON.parse(response.body)['error'], "Password can't be blank"
+  end
+
+  test 'should return errors when creating email invalid' do
+    post '/api/v1/users', params: {
+      user: {
+        name: 'John Doe',
+        email: 'john.doe@@example.com',
+        password: 'password'
+      }
+    }
+
+    assert_response :unprocessable_entity
+    assert_includes JSON.parse(response.body)['error'], "Email is invalid"
+  end
+
+end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,7 @@
 ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
+require 'simplecov'
 
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
@@ -8,6 +9,6 @@ class ActiveSupport::TestCase
 
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
-
+  SimpleCov.start
   # Add more helper methods to be used by all tests here...
 end
